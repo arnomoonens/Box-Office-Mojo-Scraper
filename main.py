d99862ffd7299659232a987fda63d6e52c4c55c4
Arno <arno.moonens@gmail.com>
2015-04-20 21:51:13 +0200

    Added support for box office numbers in multiple years and put code in a function

--- main.py
+++ main.py
@@ -3,30 +3,33 @@ import requests
 from datetime import datetime, timedelta
 import urlparse
 
-movie = raw_input("Name of movie on Box Office Mojo: ")
-url = "http://www.boxofficemojo.com/movies/?page=weekly&id=" + movie + ".htm"
+def get_box_office(movie):
+	url = "http://www.boxofficemojo.com/movies/?page=weekly&id=" + movie + ".htm"
 
-req = requests.get(url)
-soup = BeautifulSoup(req.text)
+	req = requests.get(url)
+	soup = BeautifulSoup(req.text)
+	results = []
 
-results_table = soup.find_all("table", class_="chart-wide")[0]
+	for table in soup.find_all("table", class_="chart-wide"):
+		rows = table.find_all("tr")
+		for row in rows[1:]: #First "td" has column names
+			result = []
+			link = row.td.nobr.font.a['href']
+			parsed = urlparse.urlparse(link)
+			week_of_year = int(urlparse.parse_qs(parsed.query)['wk'][0])
 
-rows = results_table.find_all("tr")
-results = []
+			year = int(table.find_previous_sibling().font.string)
+			begin_day = datetime(year, 1, 1) + timedelta(days=(7*(week_of_year-1))+1)
+			end_day = begin_day + timedelta(days=7)
+			result.append([begin_day, end_day])
 
-for row in rows[1:]: #First "td" has column names
-	result = []
-	link = row.td.nobr.font.a['href']
-	parsed = urlparse.urlparse(link)
-	week_of_year = int(urlparse.parse_qs(parsed.query)['wk'][0])
+			#Add box office
+			result.append(int(row.find_all("td")[2].string.replace(",","").replace("$", "")))
+			results.append(result)
 
-	begin_day = datetime(datetime.now().year, 1, 1) + timedelta(days=(7*(week_of_year-1))+1)
-	end_day = begin_day + timedelta(days=7)
-	result.append([begin_day, end_day])
-
-	#Add box office
-	result.append(int(row.find_all("td")[2].string.replace(",","").replace("$", "")))
-	results.append(result)
+	return results
 
 
-print "Results:", results
+if __name__ == "__main__":
+	movie = raw_input("Name of movie on Box Office Mojo: ")
+	print "Results:", get_box_office(movie)
