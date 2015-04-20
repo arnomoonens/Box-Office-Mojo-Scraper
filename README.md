b21705d1eaf6342ab267b2806f148e75fdfcca8b
Arno <arno.moonens@gmail.com>
2015-04-20 20:26:14 +0200

    Extracted datetimes from date and box office number from string

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Box Office Mojo Scraper
 Use at your own risk.
+
 Scrapes weekly box office data from Box Office Mojo.
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
+from datetime import datetime, timedelta
+import urlparse
 
-#movie = raw_input("Name of movie on Box Office Mojo: ")
-movie = "Fast7"
+movie = raw_input("Name of movie on Box Office Mojo: ")
 url = "http://www.boxofficemojo.com/movies/?page=weekly&id=" + movie + ".htm"
 
 req = requests.get(url)
@@ -13,8 +14,19 @@ results_table = soup.find_all("table", class_="chart-wide")[0]
 rows = results_table.find_all("tr")
 results = []
 
-for row in rows[1:]:
-	results.append(list(td.string for td in row.find_all("td")))
+for row in rows[1:]: #First "td" has column names
+	result = []
+	link = row.td.nobr.font.a['href']
+	parsed = urlparse.urlparse(link)
+	week_of_year = int(urlparse.parse_qs(parsed.query)['wk'][0])
+
+	begin_day = datetime(datetime.now().year, 1, 1) + timedelta(days=(7*(week_of_year-1))+1)
+	end_day = begin_day + timedelta(days=7)
+	result.append([begin_day, end_day])
+
+	#Add box office
+	result.append(int(row.find_all("td")[2].string.replace(",","").replace("$", "")))
+	results.append(result)
 
 
 print "Results:", results
